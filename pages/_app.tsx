ee8e0601786b54ca0f2b7c4806b1fb5c5e6fc33f
Tre <tre@Tres-MacBook-Air.local>
2022-08-18 15:10:05 -0500

    Feature: Decided to switch from Chakra-ui and go with Mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
 
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '../src/theme/theme'
-import Layout from '../src/components/layout/layout'
+export default function App(props: AppProps) {
+    const { Component, pageProps } = props
 
-import CmdPalete from '../src/components/cmd-palete/cmd-index'
-import CmdPaletteProvider from '../src/providers/cmd-palete-provider'
-
-function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <CmdPaletteProvider>
-                <Layout>
-                    <CmdPalete />
-                    <Component {...pageProps} />
-                </Layout>
-            </CmdPaletteProvider>
-        </ChakraProvider>
+        <>
+            <Head>
+                <title>Page title</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: 'light',
+                }}
+            >
+                <Component {...pageProps} />
+            </MantineProvider>
+        </>
     )
 }
-
-export default MyApp
